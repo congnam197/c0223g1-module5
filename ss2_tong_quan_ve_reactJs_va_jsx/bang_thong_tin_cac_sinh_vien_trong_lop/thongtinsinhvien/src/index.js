20b63401032c1d4e97d976d24b0a545849cd1eb5
congnam1997 <namhung024@gmail.com>
2023-07-19 16:44:16 +0700

    fix lỗi

--- ss2_tong_quan_ve_reactJs_va_jsx/bang_thong_tin_cac_sinh_vien_trong_lop/thongtinsinhvien/src/index.js
+++ ss2_tong_quan_ve_reactJs_va_jsx/bang_thong_tin_cac_sinh_vien_trong_lop/thongtinsinhvien/src/index.js
@@ -1,63 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const students = [
+  { id: 1, company: "CodeGym", contact: "Pham Cong Nam", country: "Nghe An" },
+  { id: 2, company: "CodeGym", contact: "Le Van Hung", country: "Da Nang" },
+  { id: 3, company: "CodeGym", contact: "Mai  Phuoc Tai", country: "Hue" },
+  { id: 4, company: "CodeGym", contact: "Helen Bennett", country: "UK" },
+  { id: 5, company: "CodeGym", contact: "Do Thanh Nhan", country: "Hue" },
   {
-    company: 'CodeGym',
-    contact: 'Pham Cong Nam',
-    country: 'Nghe An'
+    id: 6,
+    company: "CodeGym",
+    contact: "Truong Tan Hai",
+    country: "Quang Nam",
   },
-  {
-    company: 'CodeGym',
-    contact: 'Le Van Hung',
-    country: 'Da Nang'
-  },
-  {
-    company: 'CodeGym',
-    contact: 'Mai  Phuoc Tai',
-    country: 'Hue'
-  },
-  {
-    company: 'CodeGym',
-    contact: 'Helen Bennett',
-    country: 'UK'
-  },
-  {
-    company: 'CodeGym',
-    contact: 'Do Thanh Nhan',
-    country: 'Hue'
-  },
-  {
-    company: 'CodeGym',
-    contact: 'Truong Tan Hai',
-    country: 'Quang Nam'
-  }
 ];
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
     <h1>Students Information</h1>
-  <table>
-    <thead>
-      <tr>
-        <th>Company</th>
-        <th>Name</th>
-        <th>Country</th>
-      </tr>
+    <table>
+      <thead>
+        <tr>
+          <th>Company</th>
+          <th>Name</th>
+          <th>Country</th>
+        </tr>
       </thead>
       <tbody>
-     { students.map(student =>(
-        <tr>
-          <td>{student.company}</td>
-          <td>{student.contact}</td>
-          <td>{student.country}</td>
-        </tr>
-      ))}
+        {students.map((student) => (
+          <tr key={student.id}>
+            <td>{student.company}</td>
+            <td>{student.contact}</td>
+            <td>{student.country}</td>
+          </tr>
+        ))}
       </tbody>
-  </table>
+    </table>
   </div>
 );
-
